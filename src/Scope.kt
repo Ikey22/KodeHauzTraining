a6a16cb8468973e5597eb21cabb1dd394b1a3745
Ikey22 <ikechukwuachom@gmail.com>
2020-06-18 10:13:10 +0000

    new commit

---
+++ src/Scope.kt
@@ -0,0 +1,20 @@
+var name:String? = null
+var age: Int? = null
+
+fun showUserInfo(){
+    name = "Coco"
+    println("Welcome $name")
+}
+
+fun main(){
+    var myName = "Ezinne"
+    var name = "chris"
+    name = "Kingsley"
+    println(name)
+    for (i in 1..4){
+        val age = 22
+//        println(age)
+    }
+    println(age)
+    showUserInfo()
+}
